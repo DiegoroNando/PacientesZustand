f32a91d7f087b1258ab6caac8c3cc215bde08953
Diego Gallegos <diego.gallegos@swyti.com.mx>
2025-08-04 22:18:45 -0600

    Formularios Finalizados

--- src/components/PatientDetailItem.tsx
+++ src/components/PatientDetailItem.tsx
@@ -7,10 +7,9 @@ export type PatientDetailItemProps={
 
 export default function PatientDetailItem({label, data}: PatientDetailItemProps) {
   return (
-    <div>
-        <p className="font-bold mb-3 text-gray-700 uppercase"> {label}: {''}
+        <p className="font-bold mb-3 text-gray-780 uppercase"> {label}: {''}
             <span className="font-normal normal-case">{data}</span>
         </p>
-    </div>
+
   )
 }
--- src/components/PatientsDetail.tsx
+++ src/components/PatientsDetail.tsx
@@ -1,17 +1,42 @@
 import type { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
+import { usePatientStore } from "../store"
 
 type PatientsDetailProps ={
     patient: Patient
 }
 
 export default function PatientsDetail({patient}:PatientsDetailProps) {
+
+  const deletePatient = usePatientStore ((state)=> state.deletePatient )
+  const getPatientById = usePatientStore ((state)=> state.getPatientById)
   return (
-    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
-        <PatientDetailItem
-        label="ID:"
-        data={patient.id}
-        />
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl text-left">
+        <PatientDetailItem label="ID" data={patient.id} />
+        <PatientDetailItem label="Nombre"data={patient.name}/>
+        <PatientDetailItem label="Propietario" data={patient.caretaker} />
+        <PatientDetailItem label="E-mail"data={patient.email} />
+        <PatientDetailItem label="Fecha Alta" data={patient.date.toString()}/>
+        <PatientDetailItem label="Sintomas" data={patient.symptoms} />
+
+        <div className="flex justify-between mt-10">
+          <button 
+          type="button"
+          className="py-2 px-10 bg-indigo-700 hover:bg-indigo-700 text-white uppercase font-bold rounded-lg"
+          onClick={()=>getPatientById(patient.id)}
+          >
+            Editar
+          </button>
+
+          <button 
+          type="button"
+          className="py-2 px-10 bg-red-700 hover:bg-red-700 text-white uppercase font-bold rounded-lg"
+          onClick={()=> deletePatient(patient.id)}
+          >
+            Eliminar
+          </button>
+
+        </div>
     </div>
   )
 }
